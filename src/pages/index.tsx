afb02f647fd657461330cd6d5a8ba8975829865a
Alexander Kachkaev <alexander@kachkaev.ru>
2019-12-14 13:33:09 +0000

    Add twitter card data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -152,8 +152,15 @@ const IndexPage = () => {
         <meta name="description" content={description} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
         <meta
           property="og:image"
+          content={`${process.env.siteUrl}/og-image.png`}
+        />
+        <meta
+          property="twitter:image"
           content={`${process.env.siteUrl}/og-image.png`}
         />
         <meta
