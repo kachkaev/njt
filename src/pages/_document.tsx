54857e5d369b9ad175b2fd9e8336ab258853ed95
Alexander Kachkaev <alexander@kachkaev.ru>
2019-12-14 12:55:15 +0000

    Move favicon tags to _document, configure meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -26,5 +26,40 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  public render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#42a73f" />
+          <meta name="msapplication-TileColor" content="#42a73f" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -146,27 +146,11 @@ const IndexPage = () => {
   return (
     <Container>
       <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
+        <title>njt (npm jump to)</title>
+        <meta
+          name="description"
+          content="package navigation shortcuts you dreamed about"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#42a73f" />
-        <meta name="theme-color" content="#ffffff" />
       </Head>
       <GlobalStyle />
       <Title>🐸 njt 🐸</Title>
