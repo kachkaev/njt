c580970add59bc5ef731b338be14a1bfb2a1a0de
Alexander Kachkaev <alexander@kachkaev.ru>
2020-11-21 14:22:16 +0000

    Bump dependencies

--- src/ui/shared/ExternalLink.tsx
+++ src/ui/shared/ExternalLink.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export const ExternalLink: React.FunctionComponent<React.HTMLProps<
-  HTMLAnchorElement
->> = ({ children, href, ...rest }) => {
+export const ExternalLink: React.FunctionComponent<
+  React.HTMLProps<HTMLAnchorElement>
+> = ({ children, href, ...rest }) => {
   return (
     <a href={href} {...rest}>
       {children ?? href?.replace(/^https?:\/\//i, "").replace(/^www\./i, "")}
