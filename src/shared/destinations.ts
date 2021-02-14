8081a96eedd15f392be8425103303b9de284291e
Zander Martineau <hi@zander.wtf>
2021-02-14 22:40:48 +0000

    feat: add github1s

--- src/shared/destinations.ts
+++ src/shared/destinations.ts
@@ -228,6 +228,13 @@ const destinationConfigs: DestinationConfig[] = [
     keywords: ["y"],
     generateUrl: (packageName) => `https://yarnpkg.com/package/${packageName}`,
   },
+  {
+    keywords: ["1s"],
+    generateUrl: async (packageName) => {
+      const repoUrl = await getRepoUrl(packageName);
+      return repoUrl.replace("github.com", "github1s.com");
+    },
+  },
 ];
 
 const destinationConfigByKeyword: Record<
