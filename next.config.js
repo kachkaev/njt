aad52467e226247dce84366de848265ff4fa7441
Alexander Kachkaev <alexander@kachkaev.ru>
2021-11-21 20:27:23 +0000

    Revert "Use latest yarn because of https://github.com/vercel/next.js/issues/30873"
    This reverts commit d331ba53a2e2f5f8f226a741be4c3ccb74914a5d.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   experimental: {
     styledComponents: true,
+    esmExternals: false, // https://github.com/vercel/next.js/issues/30873
   },
   productionBrowserSourceMaps: true,
   rewrites: () => [
