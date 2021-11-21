cd593d0f17b4bb2651e4fefe96da71d27993caf7
Alexander Kachkaev <alexander@kachkaev.ru>
2021-11-21 20:27:02 +0000

    Use @rushstack/eslint-patch

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   extends: ["@kachkaev/eslint-config-react"],
   rules: {
     "import/no-default-export": "error",
   },
+  parserOptions: { tsconfigRootDir: __dirname },
   overrides: [
     {
       files: ["src/pages/**"],
