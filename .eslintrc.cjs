23a93cbbd873238e4e5d6ae77e7e411e3c79f185
Alexander Kachkaev <alexander@kachkaev.ru>
2022-01-03 01:25:50 +0000

    Simplify local ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,10 @@
 module.exports = {
   extends: [
-    "plugin:unicorn/recommended",
     "@kachkaev/eslint-config-react",
-    // "@kachkaev/eslint-config-base/extra-type-checking", // TODO: release and replace
+    "@kachkaev/eslint-config-react/extra-type-checking",
     "plugin:@next/next/recommended",
   ],
-  rules: {
-    "import/no-unresolved": "off", // https://github.com/import-js/eslint-plugin-import/issues/2132
-    "unicorn/prevent-abbreviations": "off",
-  },
   overrides: [
-    {
-      // Allow CommonJS modules to use `require("module-name")` and `module.exports`
-      files: ["*.cjs"],
-      env: {
-        node: true,
-      },
-      rules: {
-        "unicorn/prefer-module": "off",
-        "@typescript-eslint/no-var-requires": "off",
-      },
-    },
     {
       files: ["src/{shared,ui}/**"],
       rules: {
@@ -33,26 +17,6 @@ module.exports = {
             },
           },
         ],
-      },
-    },
-    {
-      files: ["src/commands/**"],
-      rules: {
-        "unicorn/filename-case": "off",
-      },
-    },
-    {
-      files: ["*.ts{x,}"],
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-      rules: {
-        "@typescript-eslint/no-confusing-void-expression": "error",
-        "@typescript-eslint/no-unnecessary-condition": "error",
-        "@typescript-eslint/switch-exhaustiveness-check": "error",
       },
     },
   ],
