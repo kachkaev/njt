d005354136f2977722d1f7cac9ea6fb1776c633f
Alexander Kachkaev <alexander@kachkaev.ru>
2022-01-12 09:04:39 +0000

    Fix ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   extends: [
     "@kachkaev/eslint-config-react",
-    "@kachkaev/eslint-config-base/extra-type-checking",
+    "@kachkaev/eslint-config-react/extra-type-checking",
     "plugin:@next/next/recommended",
   ],
   parserOptions: { tsconfigRootDir: __dirname },
