16e9950eb02fd7f8f383416494e3a137230da71a
Alexander Kachkaev <alexander@kachkaev.ru>
2022-02-01 20:56:51 +0000

    Yarn Berry: Switch from from node_modules to PnP  (#161)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,12 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   extends: [
     "@kachkaev/eslint-config-react",
     "@kachkaev/eslint-config-react/extra-type-checking",
     "plugin:@next/next/recommended",
   ],
+  parserOptions: { tsconfigRootDir: __dirname },
   overrides: [
     {
       files: ["src/{shared,ui}/**"],
