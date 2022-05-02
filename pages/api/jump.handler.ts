55e168b3aeb7f5c8fd15dfe63ab0a13d17c29445
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-05-02 19:42:35 +0300

    feat: add Cache-Control header for faster responses

--- pages/api/jump.handler.ts
+++ pages/api/jump.handler.ts
@@ -4,6 +4,7 @@ import { resolveDestination } from "../shared/destinations";
 
 const handler: NextApiHandler = async (req, res) => {
   let destinationUrl = "/";
+  let maxAge = 0;
 
   const to = typeof req.query.to === "string" ? req.query.to : "";
 
@@ -18,11 +19,15 @@ const handler: NextApiHandler = async (req, res) => {
 
     if (resolvedDestination.outcome === "success") {
       destinationUrl = resolvedDestination.url;
+      const queryMaxAge = Number(req.query["max-age"]);
+      maxAge = Number.isNaN(queryMaxAge) ? maxAge : queryMaxAge;
     }
   }
 
-  res.writeHead(302, {
+  res.writeHead(301, {
     Location: destinationUrl,
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    "Cache-Control": `public, max-age=${maxAge}, must-revalidate`,
   });
 
   res.end();
