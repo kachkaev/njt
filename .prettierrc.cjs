80fa5ca5a9939f3cefd7a773eeab7e2236703a5f
Alexander Kachkaev <alexander@kachkaev.ru>
2023-04-23 15:23:51 +0100

    Remove redundant config option

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,7 +1,5 @@
 /** @type {import('prettier').Options} */
 module.exports = {
-  trailingComma: "all",
-
   plugins: [
     require("prettier-plugin-packagejson"),
     require("prettier-plugin-sh"),
