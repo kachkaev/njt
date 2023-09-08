2309ed8f7996866e55073ea37fc55542cec62d4d
Alexander Kachkaev <alexander@kachkaev.ru>
2023-09-08 19:10:59 +0100

    Replace yarn with pnpm (#438)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,3 @@
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 module.exports = {
   extends: [
     "@kachkaev/eslint-config-react",
--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
 
   // We call linters in GitHub Actions for all pull requests. By not linting
   // again during `next build`, we save CI minutes and unlock more feedback.
-  // For local checks, run `yarn lint`.
+  // For local checks, run `pnpm lint`.
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 
